84051e6077a577cf6032796f43e96463a7a0e13a
galiaskarov <triumphsuccessachievment@gmail.com>
2015-07-30 19:35:29 +0600

    table view

---
+++ CustomScrollContainer/AViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AViewController.swift
+//  CustomScrollContainer
+//
+//  Created by Yan Saraev on 6/21/15.
+//  Copyright (c) 2015 nt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AViewController: UITableViewController {
+	
+	 override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		var cell:CustomTableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as! CustomTableViewCell;
+		cell.label?.text = "AController";
+		return cell;
+	}
+	
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1;
+	}
+	
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1;
+	}
+
+}
